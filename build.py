11852d59f77b0378aca081cf36c9ea550e24a685
Peter Bienstman <Peter.Bienstman@gmail.com>
2021-04-16 16:57:13 +0200

    Start on symmetry chapter.

--- build.py
+++ build.py
@@ -93,7 +93,7 @@ def build():
     # Copy directories.
     chapter_dirs = ["preamble", "helmholtz", "complex", "bessel",
                     "hermite", "numeric",
-                    "periodic", "dynamic", "postamble"]
+                    "symmetry", "dynamic", "postamble"]
     other_dirs = ["styles", "icons"]
     for chapter_dir in chapter_dirs + other_dirs:
         shutil.copytree(chapter_dir, os.path.join(build_dir, chapter_dir))
