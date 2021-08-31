80f558ba1bdb14f865b35ccaee44e942a5b7c275
Peter Bienstman <Peter.Bienstman@gmail.com>
2021-08-31 15:30:52 +0200

    Add videos to all recommended exercises.

--- build.py
+++ build.py
@@ -94,7 +94,7 @@ def build():
     chapter_dirs = ["preamble", "helmholtz", "complex", "bessel",
                     "hermite", "numeric",
                     "symmetry", "dynamic", "postamble"]
-    other_dirs = ["styles", "icons"]
+    other_dirs = ["styles", "icons"]        
     for chapter_dir in chapter_dirs + other_dirs:
         shutil.copytree(chapter_dir, os.path.join(build_dir, chapter_dir))
     # Copy files.
